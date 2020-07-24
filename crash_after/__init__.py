2f076d6779ecb2052b480c331558e1a8b2059162
Zack Kitzmiller <zackkitzmiller@gmail.com>
2020-07-23 19:18:29 -0500

    black

--- crash_after/__init__.py
+++ crash_after/__init__.py
@@ -15,6 +15,8 @@ def crash_after(func, date: str = None, *args, **kwargs):
     now = datetime.datetime.now()
     _crash_after = datetime.datetime.strptime(date, "%m/%d/%Y")
     if _crash_after < now:
-        raise CrashAfterException("This method expired in its current form on {0}".format(date))
+        raise CrashAfterException(
+            "This method expired in its current form on {0}".format(date)
+        )
 
     return func(*args, **kwargs)
